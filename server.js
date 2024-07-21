8c28556b77d9dabdd7c4d982a7e3af6558bfd28e
nikky-kumar7505 <knikki7505@gmail.com>
2024-07-21 17:12:52 +0530

    shift mongodb to onlne server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const bodyParser = require('body-parser');
+require('dotenv').config();
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
 
 
 
@@ -20,6 +23,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log('Listening on port 3000');
 });
